fbade84e7e0af7c30618c94216057a1f2b006ec4
Verbytskyi Kostiantyn <kosver0427@gmail.com>
2024-11-17 22:27:36 +0200

    Add project files.

---
+++ Kursova_BD/Models/ShopDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Kursova_BD.Models.Tables;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kursova_BD.Models
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Item> items {  get; set; }
+    }
+}
---
+++ Kursova_BD/Models/Tables/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace Kursova_BD.Models.Tables
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Balance { get; set; }
+    }
+}
